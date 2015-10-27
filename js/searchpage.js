f65942eb270f3f8efa2f43312b30489fe48f0ad7
Abhishek <asyashsingh@gmail.com>
2015-10-27 19:10:28 +0530

    commit 2

--- js/searchpage.js
+++ js/searchpage.js
@@ -2,36 +2,46 @@
  * Created by Abhishek on 11-10-2015.
  */
 
+function loaddata(page1)
+{
+    sessionStorage.page=page1;
+    window.location.href = "next.html";
+}
 
 function getdata() {
     var place=sessionStorage.place;
     var categ= sessionStorage.category;
-
+    var page= sessionStorage.page;
     //initializing parse
     Parse.initialize("Y9xqZX58mQYpEmHUphM7hFpd84LA5ba8mhJvnqNE", "iyjnAfqbh3U3MnYptBJV3KjvE93e8mdXCFbcBvQK");
     var wed = Parse.Object.extend(categ);
     var query = new Parse.Query(wed);
-    query.equalTo("address", place);
+    query.equalTo("place", place);
+    query.skip(20*(page-1));
+    query.limit(20);
     query.find({
         success: function(results) {
             var name1;
-            var address;
-            var budget;
+            var vendor;
+            var phno;
             // Do something with the returned Parse.Object values
             for (var i = 0; i < results.length; i++) {
                 var object = results[i];
-                name1=object.get('name');
-                address=object.get('address');
-                budget=object.get('cost');
-                var table = document.getElementById("mytable");
-                var row = table.insertRow(1);
-                var cell1 = row.insertCell(0);
-                var cell2 = row.insertCell(1);
-                var cell3 = row.insertCell(2);
-                cell1.innerHTML = name1;
-                cell2.innerHTML = address;
-                cell3.innerHTML = budget;
+                name1=object.get('bandname');
+                vendor=object.get('provider');
+                phno=object.get('mobno');
+
+                if(phno=="undefined"){
+                    phno=object.get("phno");
+                }
+                console.log(phno);
+                var $abc=$("#clone").clone().attr("id","clone"+i);
+                $abc.find(".pname").html(name1);
+                $abc.find(".vname").html(vendor);
+                $abc.find("#ph").html(phno);
+                $("tbody").append($abc);
             }
+
         },
         error: function(error) {
             alert("Error: " + error.code + " " + error.message);
